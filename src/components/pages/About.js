4e3b6fcf196021d1c81292a18fe0abe2957f5a72
Vikas <drupalchamp@gmail.com>
2022-07-28 18:02:09 +0530

    worked with news API

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,19 +5,29 @@ function About() {
   }, [])
   const [items, setItems] = useState([])
   const fetchItems = async () => {
-    const data = await fetch('https://fortnite-api.com/v1/banners')
+    // const data = await fetch(
+    //   'https://www.googleapis.com/youtube/v3',
+    // )
+    const data = await fetch(
+      'https://newsapi.org/v2/everything?' +
+        'q=Apple&' +
+        'from=2022-07-28&' +
+        'sortBy=popularity&' +
+        'apiKey=e2e918acec174eb3be9687622865438f',
+    )
     const items = await data.json()
-    console.log(items.data)
-    setItems(items.data)
+    console.log(items.articles)
+    setItems(items.articles)
   }
   return (
-    <div className="container d-grid mt-5">
-      {items.map((item) => (
-        <div className='items'>
-          <img src={`${item.images.smallIcon}`} alt="Icon" />
-          <p key={item.id}>
-            <strong>{item.devName}</strong>
-          </p>
+    <div className="container d-flex mt-5">
+      {items.map((articles) => (
+        <div className="items">
+          <img id="ApiImages" src={`${articles.urlToImage}`} alt="Icon" />
+          <h4>{articles.title}</h4>
+          <p>by-<strong> {articles.author}</strong></p>
+          <p>{articles.description}<a href={articles.url} target="_blank" rel="noreferrer" className="d-block btn btn-dark mt-3" title='From site'> Read More</a></p>
+          <p>Published At- {articles.publishedAt}</p>
         </div>
       ))}
     </div>
